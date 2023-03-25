da8fe2642d7ac33c5c53996125416dc855041c4e
len-kami <lkami@bogital.com>
2023-03-25 14:37:18 +0100

    - getStaticPaths fix on blog

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -169,7 +169,7 @@ export const getStaticPaths: GetStaticPaths = async (context) => {
                   postId
                   slug
                   language {
-                    locale
+                    slug
                   }
                 }
               }
